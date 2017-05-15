210913f49b196e6af98e6fad1a4321e6f0afa013
Thomas Vangoidsenhoven <thomas.vangoidsenhoven@student.ucll.be>
2017-05-15 18:29:40 +0200

    start

---
+++ js/contentswitcher.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Thomas on 25/04/2017.
+ */
+var currentIndex = 0;
+var firsttable = $("#1");
+
+$( "#switchLeft" ).click(function() {
+
+
+
+});
+
+$( "#switchRight" ).click(function() {
+    $( "span" ).show( 2000 );
+});
