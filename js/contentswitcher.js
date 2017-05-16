967af5f9952e05f0647eae9a6edf6cb6f66400d3
Thomas <thomas.vangoidsenhoven@student.ucll.be>
2017-05-16 07:57:10 +0200

    m

--- js/contentswitcher.js
+++ js/contentswitcher.js
@@ -1,15 +1,35 @@
 /**
  * Created by Thomas on 25/04/2017.
  */
-var currentIndex = 0;
-var firsttable = $("#1");
+var currentPointer = 1;
+
 
 $( "#switchLeft" ).click(function() {
+    $("#"+currentPointer).hide();
 
+    if(currentPointer == 1)
+    {
+        currentPointer = 8;
+        console.log("reset")
+    }else
+    {
+        currentPointer--;
+    }
+    $("#"+currentPointer).show();
 
 
 });
 
 $( "#switchRight" ).click(function() {
-    $( "span" ).show( 2000 );
+    $("#"+currentPointer).hide();
+
+    if(currentPointer == 8)
+    {
+        currentPointer = 1;
+        console.log("reset")
+    }else
+    {
+        currentPointer++;
+    }
+    $("#"+currentPointer).show();
 });
